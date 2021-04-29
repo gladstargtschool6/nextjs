97000184827ddf811da35bfd7f18b9e9696e838a
Joseph Moshood <gladstargtschool6@gmail.com>
2021-04-29 08:30:35 +0100

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import '@/assets/main.css';
+import '@/assets/chrome-bug.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from '@/components/Layout';
+import { UserContextProvider } from '@/utils/useUser';
+
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.body.classList?.remove('loading');
+  }, []);
+
+  return (
+    <div className="bg-primary">
+      <UserContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserContextProvider>
+    </div>
+  );
 }
-
-export default MyApp
