d7917141990dc6287f17c26d067d6261861013fe
Joseph Moshood <gladstargtschool6@gmail.com>
2021-04-29 04:21:31 +0100

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://imdvvfcbtrfewysxgrnr.supabase.co
 
 export default (req, res) => {
   res.statusCode = 200
