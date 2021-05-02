d1472831bd28e42edd1247556117d6e584ceff36
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-02 20:39:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
          }
 
          async function printProduct () {
-           products = await getProductNames();
+           products = getProductNames();
            const promises = products.map(p => getProduct(p));
            let results = [];
            results = await Promise.all(promises);
