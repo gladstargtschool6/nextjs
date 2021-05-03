0146da8aed185fd09bdb2d021559db3b8fcf1f81
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-03 16:58:35 +0100

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
-
-module.exports = {
-  module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
-  },
-  plugins: [new HtmlWebpackPlugin({ template: './pages/index.js' })],
-};
