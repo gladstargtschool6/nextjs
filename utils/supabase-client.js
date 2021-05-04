9b411554708ead9b192d7387700c6294a6326abd
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-04 13:45:53 +0100

    Update supabase-client.js

--- utils/supabase-client.js
+++ utils/supabase-client.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supasupabase-js
-
   ​const​ ​{​ createClient ​}​ ​=​ ​supabase​
   ​supabase​ ​=​ ​createClient​(​'https://imdvvfcbtrfewysxgrnr.supabase.co',​ ​'3c89de2b-ff7c-47ec-bd52-06317b777ab1'1
 export const supabase = createClient
